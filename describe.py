833f78b03b182f7d7016c74d429a4ad2b07d0315
Belen Arbizu jimenez <barbizu-@c3r5s1.42malaga.com>
2024-11-07 19:03:19 +0100

    class data_analysis

--- describe.py
+++ describe.py
@@ -1,21 +1,26 @@
 import sys
 import pandas as pd
 
-def read_file():
-    try:
-        dataset = pd.read_csv("dataset_train.csv")
-        data_dict = dataset.to_dict(orient='list')
-    except:
-        print("Coludn't read the dataset")
-        sys.exit(1)
+class DataAnalysis:
+    def __init__():
+        self.data_dict = []
+
+    def read_file():
+        try:
+            dataset = pd.read_csv("dataset_train.csv")
+            self.data_dict = dataset.to_dict(orient='list')
+        except:
+            print("Coludn't read the dataset")
+            sys.exit(1)
 
 
 def main():
     if (sys.argv < 2):
         print("Usage: python3 ./describe.py dataset_name")
         sys.exit(1)
-    else
-        read_file()
+    
+    data = DataAnalysis()
+    data.read_file()
 
 if __name__ == "__main__":
     main()
