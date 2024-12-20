7ec94d2a033b43cc24a86935a179af852dc76330
Belen Arbizu jimenez <barbizu-@c3r8s4.42malaga.com>
2024-12-20 17:45:05 +0100

    columns int data added

--- describe.py
+++ describe.py
@@ -2,25 +2,31 @@ import sys
 import pandas as pd
 
 class DataAnalysis:
-    def __init__():
-        self.data_dict = []
 
-    def read_file():
+    def __init__(self):
+        self.data_dict = []
+        self.num_data = []
+
+    def read_file(self, dataset):
         try:
-            dataset = pd.read_csv("dataset_train.csv")
-            self.data_dict = dataset.to_dict(orient='list')
+            self.data_dict = pd.read_csv(dataset, index_col="Index")
+            columns_name = []
+            for name in self.data_dict.columns.values:
+                if isinstance(self.data_dict[name].iloc[0], int) or isinstance(self.data_dict[name].iloc[0], float):
+                    columns_name.append(name)
+            self.num_data = self.data_dict.loc[:, columns_name]
         except:
             print("Coludn't read the dataset")
             sys.exit(1)
 
 
 def main():
-    if (sys.argv < 2):
+    if (len(sys.argv) < 2):
         print("Usage: python3 ./describe.py dataset_name")
         sys.exit(1)
-    
+
     data = DataAnalysis()
-    data.read_file()
+    data.read_file(sys.argv[1])
 
 if __name__ == "__main__":
     main()
