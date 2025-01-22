b063b90ad47c1e77674394f06cc65e603cd2fe2f
Belen Arbizu jimenez <barbizu-@c3r10s2.42malaga.com>
2025-01-22 17:44:33 +0100

    update describe

--- describe.py
+++ describe.py
@@ -8,28 +8,55 @@ class DataAnalysis:
         self.num_data = []
 
     def count(self, col_num):
-        len(col_num)
+        return len(col_num)
     
     def mean(self, col_num):
-        pass
+        total = 0.0
+        for num in col_num:
+            if (num == num): #evita los nan
+                total += num
+        return (total / len(col_num))
     
     def min(self, col_num):
         min_num = col_num[0]
-        for i in range(len(col_num)):
-            if col_num[i] < min_num:
-                min_num = col_num[i]
+        for num in col_num:
+            if num < min_num:
+                min_num = num
+        return min_num
     
     def max(self, col_num):
         max_num = col_num[0]
-        for i in range(len(col_num)):
-            if col_num[i] > max_num:
-                max_num = col_num[i]
+        for num in col_num:
+            if num > max_num:
+                max_num = num
+        return max_num
     
     def std(self, col_num):
-        pass
+        mean = self.mean(col_num)
+        self.nan_values(col_num, mean)
+        std = 0.0
+        for num in col_num:
+            if (num != num): #cambia los nan
+                num = mean
+            std += (num - mean) ** 2
+        return ((std / len(col_num)) ** 0.5)
     
-    def quartile(self, col_num):
-        pass
+    def quantile(self, col_num, percentage):
+        
+        #ordenar los numeros de menor a mayor
+        sorted_num = col_num
+
+        if (percentage == 25):
+            return float(sorted_num[int(len / 4)])
+        elif (percentage == 50):
+            return float(sorted_num[int(2 * len / 4)])
+        elif (percentage == 75):
+            return float(sorted_num[int(3 * len / 4)])
+
+    def nan_values(self, col_num, mean):
+        for num in col_num:
+            if num != num:
+                num = mean
 
     def read_file(self, dataset):
         try:
@@ -42,6 +69,24 @@ class DataAnalysis:
         except:
             print("Coludn't read the dataset")
             sys.exit(1)
+        
+        for i in range(len(self.num_data.columns)):
+            mean = self.mean(self.num_data.iloc[:,i].to_list())
+            self.nan_values(self.num_data.iloc[:,i].to_list(), mean)
+    
+    def print_calc(self):
+        pass
+        #for i in range(len(self.num_data.columns)):
+            #print(self.mean(self.num_data.iloc[:,i].to_list()))
+            #print(self.num_data.iloc[:,i].mean()) #prueba que salga el mismo mean
+            
+            #print(self.std(self.num_data.iloc[:,i].to_list()))
+            #print(self.num_data.iloc[:,i].std()) #prueba que salga el mismo std
+            
+            #print(self.quantile(self.num_data.iloc[:,i].to_list(), 25))
+            #print(self.quantile(self.num_data.iloc[:,i].to_list(), 50))
+            #print(self.quantile(self.num_data.iloc[:,i].to_list(), 75))
+            #print(self.num_data.iloc[:,i].quantile([.25, .5, .75])) #prueba que salga los mismos quantiles
 
 
 def main():
@@ -51,6 +96,7 @@ def main():
 
     data = DataAnalysis()
     data.read_file(sys.argv[1])
+    data.print_calc()
 
 if __name__ == "__main__":
     main()
