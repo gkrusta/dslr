aac220f8378bd91cdf77e6148330807cc1a769e2
Belen Arbizu jimenez <barbizu-@c3r8s6.42malaga.com>
2025-01-23 18:51:05 +0100

    print format init

--- describe.py
+++ describe.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+from toolkit import DataParser
 
 class DataAnalysis:
 
@@ -12,12 +13,9 @@ class DataAnalysis:
     
     def mean(self, col_num):
         total = 0.0
-        num_len = 0
         for num in col_num:
-            if (num == num): #evita los nan
-                total += num
-                num_len += 1
-        return (total / num_len)
+            total += num
+        return (total / len(col_num))
     
     def min(self, col_num):
         min_num = col_num[0]
@@ -36,12 +34,9 @@ class DataAnalysis:
     def std(self, col_num):
         mean = self.mean(col_num)
         std = 0.0
-        num_len = 0
         for num in col_num:
-            if (num == num): #cambia los nan
-                num_len += 1
-                std += (num - mean) ** 2
-        return ((std / num_len) ** 0.5)
+            std += (num - mean) ** 2
+        return ((std / len(col_num)) ** 0.5)
     
     def quicksort(self, col_num):
         if len(col_num) <= 1:
@@ -69,33 +64,35 @@ class DataAnalysis:
 
     def read_file(self, dataset):
         try:
-            self.data_dict = pd.read_csv(dataset, index_col="Index")
+            self.data_dict = DataParser.open_file(dataset)
             columns_name = []
             for name in self.data_dict.columns.values:
                 if isinstance(self.data_dict[name].iloc[0], int) or isinstance(self.data_dict[name].iloc[0], float):
                     columns_name.append(name)
+
             self.num_data = self.data_dict.loc[:, columns_name]
         except:
             print("Coludn't read the dataset")
             sys.exit(1)
-        
-        for i in range(len(self.num_data.columns)):
-            mean = self.mean(self.num_data.iloc[:,i].to_list())
-            self.nan_values(self.num_data.iloc[:,i].to_list(), mean)
+        self.num_data = DataParser.replace_nan_values(self.num_data)
+
     
     def print_calc(self):
-        pass
-        #for i in range(len(self.num_data.columns)):
-            #print(self.mean(self.num_data.iloc[:,i].to_list()))
-            #print(self.num_data.iloc[:,i].mean()) #prueba que salga el mismo mean
-            
-            #print(self.std(self.num_data.iloc[:,i].to_list()))
-            #print(self.num_data.iloc[:,i].std()) #prueba que salga el mismo std
-            
-            #print(self.quantile(self.num_data.iloc[:,i].to_list(), 25))
-            #print(self.quantile(self.num_data.iloc[:,i].to_list(), 50))
-            #print(self.quantile(self.num_data.iloc[:,i].to_list(), 75))
-            #print(self.num_data.iloc[:,i].quantile([.25, .5, .75])) #prueba que salga los mismos quantiles
+        math_func = ["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max", " ", " ", " ", " ", " "]
+        for i in range(len(self.num_data.columns)):
+            print(self.num_data.columns.values[i], end=' ')
+        for i in range(len(self.num_data.columns)):
+            print('{0} {1:.6f} {2:.6f} {3:.6f} {4:.6f} {5:.6f} {6:.6f} {7:.6f} {8:.6f}'.format(
+                math_func[i],
+                self.count(self.num_data.iloc[:,i].to_list()),
+                self.mean(self.num_data.iloc[:,i].to_list()),
+                self.std(self.num_data.iloc[:,i].to_list()),
+                self.min(self.num_data.iloc[:,i].to_list()),
+                self.quantile(self.num_data.iloc[:,i].to_list(), 25),
+                self.quantile(self.num_data.iloc[:,i].to_list(), 50),
+                self.quantile(self.num_data.iloc[:,i].to_list(), 75),
+                self.max(self.num_data.iloc[:,i].to_list())
+            ))
 
 
 def main():
