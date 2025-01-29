138ca501164b1522d0e21d16deb9930b4d87b95e
gkrusta <krustagabriela@gmail.com>
2025-01-29 15:25:50 +0100

    feat: histogram works in 1 subplot

--- histogram.py
+++ histogram.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from toolkit import DataParser
 
 class Histogram():
@@ -30,23 +29,31 @@ class Histogram():
     def histogram(self, infile, house_col = "Hogwarts House"):
         all_data = DataParser.open_file(infile)
         columns_to_drop = ['First Name', 'Last Name', 'Birthday', 'Best Hand']
-        data = all_data.drop(columns=columns_to_drop)
+        data = all_data.drop(columns=columns_to_drop, errors='ignore')
         self.data =  DataParser.replace_nan_values(data)
         houses = self.data[house_col].unique()
         #self.data.reset_index(inplace=True)
         
         features = self.data.drop(columns=['Hogwarts House'] ).columns
         print("features ", features)
-        rows = 3
-        cols = 5
-        fig, axs = plt.subplot(rows, cols)
-        for feature in features:
-            plt.figure(figsize=(10, 8))
+        rows, cols = 3, 5
+        fig, axs = plt.subplots(rows, cols, figsize=(12,8))
+        axs = axs.flatten()
+        for i, feature in enumerate(features):
             for house in houses:
                 house_data = self.data[self.data[house_col] == house]
-                print("house_data", house_data)
-                plt.hist(house_data[feature], bins=20, alpha = 0.7, label=house)
-            self.visualize(feature) 
+                #print("house_data", house_data)
+                axs[i].hist(house_data[feature], bins=20, alpha = 0.7, label=house)
+            axs[i].set_title(feature, fontsize=10, fontweight="bold", pad=2)
+            #axs[i].set_xlabel("Values", fontsize=6, fontweight="bold", labelpad=1)
+            #axs[i].set_ylabel("Frequency", fontsize=6, fontweight="bold", labelpad=1)
+            #self.visualize(feature)
+        h, l = axs[0].get_legend_handles_labels()
+        #plt.legend(h, l, loc='center')
+        fig.text(0.5, 0.04, 'common X', ha='center', fontweight="bold")
+        fig.text(0.04, 0.5, 'common Y', va='center', rotation='vertical', fontweight="bold")
+        fig.legend(h, l, loc='upper right', bbox_to_anchor=(1.01, 1))
+        plt.show()
 
 
 def main():
