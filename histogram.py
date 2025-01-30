7468b2d9e75ed2a6a72dada5dfb4f391930e731e
gkrusta <krustagabriela@gmail.com>
2025-01-30 12:19:11 +0100

    feat: histogram cleaned

--- histogram.py
+++ histogram.py
@@ -1,29 +1,10 @@
 import matplotlib.pyplot as plt
 from toolkit import DataParser
 
+
 class Histogram():
     def __init__(self):
         self.data = None
-
-
-    def visualize(self, feature):
-        """
-        Visualize the data distribution and optionally the regression line.
-        """
-        try:
-            plt.xlabel("Values", fontsize=14, fontweight="bold", labelpad=20)
-            plt.ylabel('Frequency', fontsize=14, fontweight="bold", labelpad=20)
-            plt.legend(loc="upper right", fontsize=10, title='Hogwarts House')
-            plt.xticks(fontsize=14)
-            plt.yticks(fontsize=14)
-            plt.title(feature, fontsize=20, fontweight="bold", pad=20)
-            plt.grid(True)
-            plt.show()
-            plt.close()
-        except KeyboardInterrupt:
-            print("\nVisualization interrupted by the user. Exiting cleanly.")
-        except Exception as e:
-            print(f"Error: {e}")
 
 
     def histogram(self, infile, house_col = "Hogwarts House"):
@@ -31,29 +12,29 @@ class Histogram():
         columns_to_drop = ['First Name', 'Last Name', 'Birthday', 'Best Hand']
         data = all_data.drop(columns=columns_to_drop, errors='ignore')
         self.data =  DataParser.replace_nan_values(data)
-        houses = self.data[house_col].unique()
-        #self.data.reset_index(inplace=True)
-        
+        houses = self.data[house_col].unique()        
         features = self.data.drop(columns=['Hogwarts House'] ).columns
-        print("features ", features)
         rows, cols = 3, 5
         fig, axs = plt.subplots(rows, cols, figsize=(12,8))
+        fig.suptitle("Histogram", fontsize=16, fontweight="bold")
         axs = axs.flatten()
+
         for i, feature in enumerate(features):
             for house in houses:
                 house_data = self.data[self.data[house_col] == house]
-                #print("house_data", house_data)
                 axs[i].hist(house_data[feature], bins=20, alpha = 0.7, label=house)
             axs[i].set_title(feature, fontsize=10, fontweight="bold", pad=2)
-            #axs[i].set_xlabel("Values", fontsize=6, fontweight="bold", labelpad=1)
-            #axs[i].set_ylabel("Frequency", fontsize=6, fontweight="bold", labelpad=1)
-            #self.visualize(feature)
+            axs[i].tick_params(axis='both', labelsize=6)
+
         h, l = axs[0].get_legend_handles_labels()
-        #plt.legend(h, l, loc='center')
-        fig.text(0.5, 0.04, 'common X', ha='center', fontweight="bold")
-        fig.text(0.04, 0.5, 'common Y', va='center', rotation='vertical', fontweight="bold")
-        fig.legend(h, l, loc='upper right', bbox_to_anchor=(1.01, 1))
+        for i in range(len(features), len(axs)):
+            axs[i].set_visible(False)
+
+        fig.text(0.5, 0.04, 'Values', ha='center', fontweight="bold")
+        fig.text(0.04, 0.5, 'Frequency', va='center', rotation='vertical', fontweight="bold")
+        fig.legend(h, l, loc='lower right', fontsize=12, bbox_to_anchor=(0.85, 0.15))
         plt.show()
+        plt.close()
 
 
 def main():
