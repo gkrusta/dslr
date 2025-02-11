a1f63ea21b45547a592a556892e21584feb327a0
gkrusta <krustagabriela@gmail.com>
2025-02-11 17:19:34 +0100

    feat: pairplot done

--- pair_plot.py
+++ pair_plot.py
@@ -26,7 +26,7 @@ class PairPlot():
         #sns.pairplot(self.data, hue=house_col)
         num_features = self.data.select_dtypes(include=['int64', 'float64']).columns
         print(num_features)
-        fig, axs = plt.subplots(len(num_features), len(num_features), figsize=(30, 30))
+        fig, axs = plt.subplots(len(num_features), len(num_features), figsize=(30, 34))
         fig.suptitle("Pair plot", fontsize=16, fontweight="bold")
 
         for i in range(len(num_features)):
@@ -38,16 +38,19 @@ class PairPlot():
                 else:
                     for house in houses:
                         house_data = self.data[self.data[house_col] == house]
-                        axs[i, j].scatter(house_data[num_features[i]], house_data[num_features[j]], label=house)
-                if j != 0:
+                        axs[i, j].scatter(house_data[num_features[i]], house_data[num_features[j]], label=house, s=5)
+                if j == 0:
+                    axs[i, j].set_ylabel(num_features[i], fontsize=8, fontweight="bold")
+                else:
                     axs[i, j].set_yticklabels([])
                 if i != len(num_features) - 1:
                     axs[i, j].set_xticklabels([])
+                elif i == len(num_features) - 1:
+                    axs[i, j].set_xlabel(num_features[j], fontsize=8, fontweight="bold")
 
-        # Add a single legend outside the plot
-        handles = [plt.Line2D([0], [0], marker='o', color='w')]
-        fig.legend(handles, houses, loc='upper right')
-        plt.tight_layout()
+        h, l = axs[0,0].get_legend_handles_labels()
+        fig.legend(h, l, loc='upper right', bbox_to_anchor=(0.98, 0.995), fontsize=12)
+        plt.tight_layout(rect=[0, 0.03, 0.99, 0.95])
         plt.show()
         plt.close()
 
